5d3b4a154d550982258e9d733770045649c5cdeb
brandonlmorris <brandon.morris95@gmail.com>
2018-02-01 12:07:14 -0600

    Add everything for the prototype

--- aufm/app.py
+++ aufm/app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 aufm = Flask(__name__)
 
 
 @aufm.route('/')
-def root():
-    return 'Hello, world!'
+def root(name='world'):
+    return render_template('index.html', name=name)
+
+
+@aufm.route('/shelby')
+def shelby():
+    return render_template('shelby-assets.html')
+
+@aufm.route('/edit-H1456253')
+def asset_edit():
+    return render_template('edit-asset.html')
+
 
 
 aufm.run()
