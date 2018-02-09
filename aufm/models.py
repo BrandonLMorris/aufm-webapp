268cb21250726d735d7b787da70d2d803d8fd8aa
brandonlmorris <brandon.morris95@gmail.com>
2018-02-09 15:25:32 -0600

    Create basic ORM with temporary sqlite db

---
+++ aufm/models.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, Integer, String, SmallInteger
+from aufm.database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    email = Column(String(128))
+    password = Column(String(64))
+    permissions = Column(SmallInteger)
+
+    def __init__(self, first_name=None, last_name=None, email=None, password=None,
+                 permissions=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.permissions = permissions
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
+
+
+class Protocol(Base):
+    __tablename__ = 'protocols'
+    id = Column(Integer, primary_key=True) # autoincrement is automatic
+    element_id = Column(Integer)
+    property = Column(String(64))
+    value = Column(String(512))
+
+    def __init__(self, id=None, element_id=None, property=None, value=None):
+        self.id = id
+        self.element_id = element_id
+        self.property = property
+        self.value = value
+
+    def __repr__(self):
+        return '<Protocol {}:{}'.format(self.id, self.element_id)
+
