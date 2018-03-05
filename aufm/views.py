6cbc8a4b034f26fc7f526b2d4781b50d3880ec24
Matt Bonsall <matt.bonsall.v@gmail.com>
2018-03-04 22:43:50 -0600

    Implemented a way to go straight to a part

--- aufm/views.py
+++ aufm/views.py
@@ -217,7 +217,12 @@ def get_parts_in_building(building_id=None, name=None):
 
 @app.route('/')
 def index():
-    return render_template('aufm.html')
+    if "part" in request.args:
+        part = request.args["part"]
+    else:
+        part = -1
+
+    return render_template('aufm.html', part=part)
 
 
 def _error(message, code):
