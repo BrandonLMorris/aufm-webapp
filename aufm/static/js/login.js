a91dba511bb62964004699663f652a8c627ac296
kinztechcom <allen@kinztech.com>
2018-04-19 14:13:16 -0500

    Forgot password feature

--- aufm/static/js/login.js
+++ aufm/static/js/login.js
@@ -3,6 +3,8 @@ AUFM = {
         setup: function() {
             $('#login_button').click(this.events.login);
             $('#forgot_button').click(this.events.forgot);
+            $('#forgot_submit').click(this.events.submit);
+            $('#forgot_cancel').click(this.events.cancel);
         },
         events: {
             login: function(e) {
@@ -30,7 +32,32 @@ AUFM = {
                 });
             },
             forgot: function(e) {
-
+                $('#email_row, #login_buttons').hide();
+                $('#forgot_buttons').show();
+            },
+            submit: function(e) {
+                var email = $('#login_email').val();
+                if(email.length == 0) {
+                    $('#login_email').addClass("invalid");
+                    return;
+                }
+                AUFM.Util.api({
+                    url: "request-password-reset",
+                    type: "POST",
+                    data: { "email": email },
+                    callback: function(data) {
+                        if(data.error) {
+                            console.log(data.error);
+                            return;
+                        }
+                        Materialize.toast('Password reset sent!', 4000);
+                        AUFM.Login.events.cancel();
+                    },
+                });
+            },
+            cancel: function(e) {
+                $('#email_row, #login_buttons').show();
+                $('#forgot_buttons').hide();
             },
         },
     },
